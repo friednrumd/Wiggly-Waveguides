6f6b81ff2ea41ae2d43ce27e926b0f8a5fec0e6e
Nate PC <friednr@umd.edu>
2024-11-13 00:35:21 -0500

    Nate's PC Commit at 13-Nov-2024 00:35:21

--- Functions/ParameterUpdateFirst.m
+++ Functions/ParameterUpdateFirst.m
@@ -2,7 +2,7 @@ function [dn,dnu] = ParameterUpdateFirst(n0, nu0, w, Hxz0, Hy0)
 
 
     Hr=(nu0.')*(-w^(-2)*(Hxz0)+Hy0)*(nu0);
-    gammar=( (1-eye(length(n0)) )./(n0-n0'+10^-20)).*Hr;
+    gammar=( (1-eye(length(n0)) )./(n0-n0'+100*eye(length(n0)))).*Hr;
     
     dn=diag(Hr);
     dnu=-gammar*nu0;
