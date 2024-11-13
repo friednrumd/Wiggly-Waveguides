1e87e7cbedf0f7517ae00cf3aebccb248e367ff8
Nate PC <friednr@umd.edu>
2024-11-13 01:02:35 -0500

    Nate's PC Commit at 13-Nov-2024 01:02:35

--- Functions/Oak.m
+++ Functions/Oak.m
@@ -1,6 +1,7 @@
-function q = scream()
+function [] = scream()
 
 fprintf('Oak\''s words echoed... "There''s a time and place for everything! But not now." \n')
+
 
 end
 
--- Functions/Perturber1.m
+++
@@ -1,7 +0,0 @@
-function [dvec,deig] = Perturber1(Hamiltonian,vecs,eigs)
-   
-deig=+Hamiltonian;
-dvec=-(     vecs'*Hamiltonian*vecs    )/(       eigs-eigs'+diag(Inf*(1:length(eigs)))       );
-
-end
-
