3fd8f297816746bfb3f3e4f4d92033abd569ea7a
Nate PC <friednr@umd.edu>
2024-11-13 01:05:22 -0500

    Nate's PC Commit at 13-Nov-2024 01:05:22

--- Functions/Oak.m
+++ Functions/Oak.m
@@ -1,6 +1,12 @@
 function [] = scream()
 
-fprintf('Oak\''s words echoed... "There''s a time and place for everything! But not now." \n')
+if rand()>0.5
+
+    fprintf('Oak\''s words echoed... "There''s a time and place for everything! But not now." \n')
+else
+    fprintf('Professor Oak: Nate! This isn''t the time to use that!" \n')
+end
+
 
 
 end
